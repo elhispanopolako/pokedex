f0c2ecd73296413e863c42c43e2bbc6aba81a052
twozniak <twozniak@consdata.com>
2023-01-13 22:51:41 +0100

    add a search and a option to find by a range of ids

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const poke_container = document.getElementById('poke-container')
-const pokemon_count = 1000
+const searchBtn = document.querySelector('.btn')
+const input = document.querySelector('.input');
+const listId = document.querySelectorAll('.pokeIds li')
+let pokemon_initCount = 1
+let pokemon_count = 300
 const colors = {
     fire: '#FDDFDF',
     grass: '#DEFDE0',
@@ -18,14 +22,20 @@ const colors = {
 }
 const main_types = Object.keys(colors)
 const fetchPokemons = async () => {
-    for (let i = 1; i <= pokemon_count; i++) {
+    for (let i = pokemon_initCount; i <= pokemon_count; i++) {
         await getPokemon(i)
     }
 }
 const getPokemon = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`
     const res = await fetch(url)
+    if (res.status == '404') {
+        createErrorCard('Pokemon not found')
+        setTimeout(() => document.location.reload(), 4000)
+
+    }
     const data = await res.json()
+
     createPokemonCard(data)
 }
 const createPokemonCard = (pokemon) => {
@@ -52,6 +62,42 @@ const createPokemonCard = (pokemon) => {
     pokemonEl.innerHTML = pokemonInnerHtml
     poke_container.appendChild(pokemonEl)
 }
+searchBtn.addEventListener('click', () => {
+    const searchValue = input.value
+    if (searchValue.trim() == '') {
+        alert('Please enter a pokemon name')
+    }
+    searchPokemon(searchValue)
+})
+const searchPokemon = (pokemon) => {
+    clearPokemons()
+    getPokemon(pokemon)
+}
+const clearPokemons = () => {
+    const pokemons = document.querySelectorAll('.pokemon')
+    pokemons.forEach(pokemon => pokemon.remove())
+}
+const createErrorCard = (msg) => {
+    const cardHtml = `<div class="card">
+    <h1>${msg}</h1>
+    </div>`
+    poke_container.innerHTML = cardHtml
+}
 
+
+
+
+listId.forEach(list => {
+    list.addEventListener('click', () => {
+        const text = list.textContent
+        const findASeparator = text.indexOf('-')
+        pokemon_initCount = text.slice(0, findASeparator)
+        pokemon_count = text.slice(findASeparator + 1)
+        console.log(pokemon_initCount, pokemon_count)
+        clearPokemons()
+        fetchPokemons()
+    })
+
+})
 
 fetchPokemons()
